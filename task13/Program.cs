51fa0741d53bf0a88d9d0fe2e092afe23a14f489
Dmitriy <sctoss@gmail.com>
2022-07-15 19:15:26 +0300

    Completed task13

---
+++ task13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n < 100)
+{
+    Console.WriteLine("Число не содержит трех знаков");
+}
+else
+{
+    while (n > 1000)
+    {
+        n = n / 10;
+    }
+    int findThirdDigit = n % 10;
+    Console.WriteLine("Третьей цифрой в числе является " + findThirdDigit);
+
+}
